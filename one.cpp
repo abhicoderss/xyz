c2143b453d17806f479ce8a02418950bbd8a38cd
Mohiti7206 <113987768+Mohiti7206@users.noreply.github.com>
2022-10-23 19:56:57 +0530

    This is by mohiti7206
    Dhhg

--- one.cpp
+++ one.cpp
@@ -5,14 +5,14 @@ int main() {
 
   int first_number, second_number, sum;
     
-  cout << "Enter njghkghkfkhbgkjdjhcvjhkthfjwgrfgfdgo integers: ";
+  cout << "Enter njghkghkfkhbgkjdjhcvjhkthfjwo integers: ";
   cin >> first_number >> second_number;
 
-  // sum of two numbers in stored in the variable sumOfTwoNumbers
+  // sum of two numbers in stored in variable sumOfTwoNumbers
   sum = first_number + second_number;
 
   // prints sum 
-  cout << first_number << " + " <<  second_number << " = " << sum;     
+  cout << first_number gjjhh mohiti7206 << " + " <<  second_number << " = " << sum;     
 
   return 0;
 }
